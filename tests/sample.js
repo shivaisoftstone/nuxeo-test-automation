cf8f8d569c5424598e2dcc8473c6ee1ee0f1b84c
shiva poosa <shivaramakrishna.poosa@isoftstone.com>
2025-08-11 13:07:26 +0530

    Addded cookies logic to launch the browser

--- tests/sample.js
+++ tests/sample.js
@@ -9,12 +9,15 @@ test.describe('Log into Nuxeo page', () => {
   // page objects declaration
   let loginPage;
   let samplePage;
+  let userType = "admin"; // or "amp" based on your requirement
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     samplePage = new SamplePage(page);
     const url = process.env[process.env.ENVIRONMENT + "_BASE_URL"]
     await page.goto(url);
+    await loginPage.setCookiesFromFile(userType);
+    await page.goto("/nuxeo/catalog");
   });
 
   test.afterEach(async ({ page }, testInfo) => {
